ad603c4b4040bfd452b8f16cdf03ffaae2d9dda0
Администратор <george2066@yandex.ru>
2022-09-10 20:33:38 +0300

    Моя курсовая

---
+++ src/Employy.java
@@ -0,0 +1,77 @@
+import javax.swing.plaf.DesktopPaneUI;
+import java.util.Objects;
+
+public class Employy {
+
+    /*ПЕРЕМЕННЫЕ*/
+
+    private String fullName;
+    private int department;
+    private int salary;
+    private static int number = 0;
+    private int id;
+
+
+
+    /*КОНСТРУКТОР*/
+
+    public Employy(String FIO, int salary, int department) {
+        if(department < 1 || department > 5) throw new IllegalArgumentException("У нас нет такого отдела.");
+        this.fullName = FIO;
+        this.salary = salary;
+        this.department = department;
+        this.id = ++number;
+    }
+
+
+
+    /*GETTER`S*/
+
+    public int getDepartment() {
+        return department;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public String getFIO() {
+        return fullName;
+    }
+    public int getId() {
+        return id;
+    }
+
+
+
+    /*SETTER`S*/
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+
+    /*МЕТОДЫ*/
+
+    @Override
+    public String toString() {
+        return "Employy{" +
+                "fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employy)) return false;
+        Employy employy = (Employy) o;
+        return department == employy.department && salary == employy.salary && id == employy.id && fullName.equals(employy.fullName);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, department, salary, id);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    private static Employy[] employys = new Employy[10];
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
